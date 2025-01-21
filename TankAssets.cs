bef79a4e18b470611cc53cd9e9de62e579928351
kanazimasang <128916318+kanazimasang@users.noreply.github.com>
2025-01-22 00:51:03 +0800

    Create TankAssets.cs

---
+++ TankAssets.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankAssets : MonoBehaviour
+{
+    public GameObject Tanknum1;
+    public GameObject Tanknum2;
+    public GameObject Tanknum3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
